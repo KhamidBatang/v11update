44bc2fa994e2485b964f8a7ac50e451f575d200f
KhamidBatang <111238475+KhamidBatang@users.noreply.github.com>
2022-12-09 14:32:40 +0700

    Update and rename plugins/donasi.js to plugin/donasi.js

--- plugin/donasi.js
+++ plugin/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+│ •  [6281947707409
+│ •  [6281947707409]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
